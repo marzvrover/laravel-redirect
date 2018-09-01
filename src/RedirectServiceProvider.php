01b856cd44e9bfd3ecd0b1de79920f4420026a7c
Matthew Dooley <Matthew@Dooley.xyz>
2018-09-01 16:18:51 -0500

    init

---
+++ src/RedirectServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+namespace MDooley47\Redirect;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\ServiceProvider;
+
+use MDooley47\UrlValidator\UrlValidator;
+
+class RedirectServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return string
+     */
+    public function boot()
+    {
+        RedirectResponse::macro('defaultTarget', function($url, $options = null) {
+            if (($target = $this->getTargetUrl() == null) || (! UrlValidator::match($url,
+                    $options)))
+            {
+                $this->setTargetUrl($url);
+            }
+        });
+    }
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
